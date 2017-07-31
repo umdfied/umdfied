73bfc2d2fa9294ea5b3039e76445c049a82eb185
uniibu <unibtc@gmail.com>
2017-07-31 19:29:47 +0000

    app.json

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-require('./src').start();
+
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+  let workerCount = +(process.env.WORKERS || require('os').cpus().length);
+
+  while (workerCount--) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', deadWorker => {
+    let worker = cluster.fork();
+    console.log(`Worker ${deadWorker.process.pid} replaced by ${worker.process.pid}`);
+  });
+} else {
+  console.log(`Starting worker ${process.pid}`);
+  require('./src').start();
+}
